a4e8891349a6a2d9bfb1f46d5f0a2b8cce08c437
xarrer <noogler22@gmail.com>
2020-06-17 21:21:07 +0545

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,9 +1,13 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "f991983e4b74ce3d25e0a253740ca595",
+  "assets/AssetManifest.json": "4ede4cc92de843e1b60fd58b92102174",
+"assets/assets/fonts/OpenSans-Bold.ttf": "1025a6e0fb0fa86f17f57cc82a6b9756",
+"assets/assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+"assets/assets/fonts/OpenSansCondensed-Bold.ttf": "cfd3dce57f28f5955c90fc5b5385d4b3",
+"assets/assets/fonts/OpenSansCondensed-Light.ttf": "3589bddbe338e444d408f4dbc545ca1e",
 "assets/assets/images/logo.png": "2900fab6285d42a3fd357894278c9d8e",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "2d1652f294897a69457b966ebf1b2981",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "7f1df1ee47854388e684afc0845ea820",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
@@ -12,7 +16,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "6de54127d381bbc97d66a8b8de2c0536",
 "/": "6de54127d381bbc97d66a8b8de2c0536",
-"main.dart.js": "2fef58a02a3204ec7098b3759ac022db",
+"main.dart.js": "6b424e626ea8a42ad4252a0590038e77",
 "manifest.json": "15cd1edee6705c95fb2939934f1bb5cf"
 };
 
